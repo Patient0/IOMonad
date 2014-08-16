f5d32c5404ead7532b04466ce44dc837f183966c
Paul Hollingsworth <paul@paulhollingsworth.com>
2014-08-16 11:32:22 +0100

    Initial version

---
+++ IOMonad/FunctionalProgram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IOMonad
+{
+    internal delegate IOAction Operation(string input);
+
+    interface IOAction
+    {
+        IOAction bind(Operation operation);
+        IOAction wrap(string text);
+    }
+
+    interface Runtime
+    {
+        IOAction readLine();
+        IOAction writeLine(string text);
+    }
+
+    class FunctionalProgram
+    {
+        public static readonly Runtime rt = RuntimeImpl.Instance;
+
+        public static IOAction Main =
+            rt.writeLine("Enter your name").
+                bind(dummy => rt.readLine()).
+                bind(name => rt.writeLine("Hello " + name + ". It's nice to meet you. Press return to exit")).
+                bind(dummy => rt.readLine());
+    }
+}
---
+++ IOMonad/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IOMonad
+{
+    abstract class RuntimeAction : IOAction
+    {
+        public IOAction bind(Operation operation)
+        {
+            return new CombinedAction(this, operation);
+        }
+
+        public IOAction wrap(string text)
+        {
+            return new Wrapped(text);
+        }
+
+        public abstract string perform();
+    }
+
+    class ReadLine : RuntimeAction
+    {
+        public override string perform()
+        {
+            return Console.ReadLine();
+        }
+    }
+
+    class WriteLine : RuntimeAction
+    {
+        readonly string text;
+        public WriteLine(string text)
+        {
+            this.text = text;
+        }
+        public override string perform()
+        {
+            Console.WriteLine(text);
+            return "";
+        }
+    }
+
+    class Wrapped : RuntimeAction
+    {
+        readonly string text;
+        public Wrapped(string text)
+        {
+            this.text = text;
+        }
+        public override string perform()
+        {
+            return text;
+        }
+    }
+
+    class CombinedAction : RuntimeAction
+    {
+        readonly RuntimeAction first;
+        readonly Operation next;
+        public CombinedAction(RuntimeAction first, Operation next)
+        {
+            this.first = first;
+            this.next = next;
+        }
+
+        public override string perform()
+        {
+            var result = first.perform();
+            var nextAction = next(result);
+            return ((RuntimeAction)nextAction).perform();
+        }
+    }
+
+    class RuntimeImpl : Runtime
+    {
+        private readonly IOAction readLn = new ReadLine();
+
+        public IOAction readLine()
+        {
+            return readLn;
+        }
+
+        public IOAction writeLine(string text)
+        {
+            return new WriteLine(text);
+        }
+
+        public static Runtime Instance = new RuntimeImpl();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ((RuntimeAction)FunctionalProgram.Main).perform();
+        }
+    }
+}
